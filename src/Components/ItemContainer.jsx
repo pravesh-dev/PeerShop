122f6ea7d196935f45272934d88292cd27285579
pravesh dav <humantorch1932@gmail.com>
2024-05-24 17:20:16 +0530

    responsive the item section for both arrival and rated

--- src/Components/ItemContainer.jsx
+++ src/Components/ItemContainer.jsx
@@ -5,12 +5,12 @@ import TopRated from './TopRated'
 function ItemContainer() {
     const [activeTab, setActiveTab] = useState('arrivals');
   return (
-    <div className='w-full min-h-screen bg-[#111] border py-5 flex flex-col gap-4'>
-      <div className='w-full flex justify-center items-center gap-5 border font-krona'>
-        <button className={`text-sm ${activeTab === 'arrivals' ? 'text-white border-b-2 border-white' : 'text-[#646464] border-none'}`} onClick={()=>{setActiveTab("arrivals")}}>New Arrivals</button>
-        <button className={`text-sm ${activeTab === 'rated' ? 'text-white border-b-2 border-white' : 'text-[#646464] border-none'}`} onClick={()=>{setActiveTab("rated")}}>Top Rated</button>
+    <div className='w-full min-h-[50vh] bg-[#111] py-5 flex flex-col gap-4 lg:gap-10'>
+      <div className='w-full flex justify-center items-center gap-5 font-krona md:gap-14 lg:gap-24'>
+        <button className={`text-sm ${activeTab === 'arrivals' ? 'text-white border-b-2 border-white' : 'text-[#646464] border-none'} md:text-base lg:text-lg`} onClick={()=>{setActiveTab("arrivals")}}>New Arrivals</button>
+        <button className={`text-sm ${activeTab === 'rated' ? 'text-white border-b-2 border-white' : 'text-[#646464] border-none'} md:text-base lg:text-lg`} onClick={()=>{setActiveTab("rated")}}>Top Rated</button>
       </div>
-      <div className='w-full'>
+      <div className='w-full lg:flex lg:justify-center'>
         { activeTab === "arrivals" ? <NewArrivals/> : <TopRated/>}
       </div>
     </div>
--- src/Components/TopRated.jsx
+++ src/Components/TopRated.jsx
@@ -6,11 +6,11 @@ function TopRated() {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
-        let response = await fetch("/newArrival.json");
+        let response = await fetch("/topRated.json");
         let data = await response.json();
         setItems(data);
         setLoading(false);
@@ -25,38 +25,26 @@ function TopRated() {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>There was an error loading the new arrivals.</p>;
+
   return (
-    <div className="w-full p-2 flex flex-wrap justify-center gap-2 font-krona">
+    <div className="w-full p-2 flex flex-wrap justify-center gap-2 font-krona lg:justify-start lg:w-[61.6rem] lg:p-0 xl:w-[65.5rem] xl:gap-7">
       {items.map((item, index) => {
         return (
-          <div key={index} className="w-36 h-48 bg-[#090909] flex flex-col items-start justify-between p-3">
-            <img className="w-32" src={item.img} alt={item.name} />
-            <h2 className="text-[0.38rem]">{item.name}</h2>
-            <button className="bg-[#4f717949] text-[#3598AF] text-[0.45rem] px-2 py-[0.04rem] rounded-sm">
+          <div key={index} className="w-36 h-48 bg-[#090909] flex flex-col items-start justify-between p-3 sm:w-52 sm:h-[17rem] lg:w-60 lg:h-80">
+            <img className="w-32 sm:w-48 lg:w-56" src={item.img} alt={item.name} />
+            <h2 className="text-[0.38rem] sm:text-[0.58rem] lg:text-[0.7rem]">{item.name}</h2>
+            <button className="bg-[#4f717949] text-[#3598AF] text-[0.45rem] px-2 py-[0.04rem] rounded-sm sm:text-[0.60rem] sm:px-4 sm:py-[0.14rem]">
               {item.rating}
             </button>
             <div className="w-full flex justify-between items-center">
-              <span className="text-[0.52rem]">${item.price}</span>{" "}
-              <button className="text-[0.3rem] bg-[#0039CA] px-3 py-[0.3rem] rounded-sm">
+              <span className="text-[0.52rem] sm:text-[0.8rem]">${item.price}</span>{" "}
+              <button className="text-[0.3rem] bg-[#0039CA] px-3 py-[0.3rem] rounded-sm sm:text-[0.4rem] sm:px-4 sm:py-[0.4rem]">
                 Add to cart
               </button>
             </div>
           </div>
         );
       })}
-      <div className="w-36 h-48 bg-[#090909] flex flex-col items-start justify-between p-3">
-        <img className="w-32" src='' alt="" />
-        <h2 className="text-[0.38rem]">Classic School Backpack</h2>
-        <button className="bg-[#4f717949] text-[#3598AF] text-[0.45rem] px-2 py-[0.04rem] rounded-sm">
-          4.1
-        </button>
-        <div className="w-full flex justify-between items-center">
-          <span className="text-[0.52rem]">$45.12</span>{" "}
-          <button className="text-[0.3rem] bg-[#0039CA] px-3 py-[0.3rem] rounded-sm">
-            Add to cart
-          </button>
-        </div>
-      </div>
     </div>
   );
 }
