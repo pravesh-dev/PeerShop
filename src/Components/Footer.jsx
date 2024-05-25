82566ae6cedfcbbd1ded3fe37672dc665c6f3b70
pravesh dav <humantorch1932@gmail.com>
2024-05-25 20:18:33 +0530

    added hover animations in footer and controled duplicat classes in footer using apply in index.css

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,44 +11,44 @@ function Footer() {
   return (
     <div className="w-full bg-[#171717] px-3 py-4 pt-10 flex flex-col gap-4 lg:gap-8 lg:px-14">
       <div className="w-full flex flex-col gap-5 items-start justify-between relative sm:flex-row sm:justify-start sm:gap-9 lg:gap-32">
-        <div className="flex items-center gap-1 absolute top-1 right-1 text-white/60"><span><AiOutlineGlobal /></span> English</div>
+        <div className="flex items-center gap-1 absolute top-1 right-1 text-white/60 cursor-pointer duration-300 hover:text-cyan-200"><span><AiOutlineGlobal /></span> English</div>
         <div className="flex flex-col gap-1">
           <h2 className="text-2xl lg:text-3xl">Contact Info</h2>
           <p className="flex items-center gap-2 lg:gap-1">
             <span>
               <IoLocationSharp color="#FF6767" />
             </span>{" "}
-            <span className="text-xs font-barlowLight lg:text-[0.6rem]">300 west cory streen</span>
+            <span className="text-xs font-barlowLight lg:text-[0.6rem] duration-300 hover:translate-x-1">300 west cory streen</span>
           </p>
           <p className="flex items-center gap-2 lg:gap-1">
             <span>
               <IoMdCall color="#FF6767" />
             </span>{" "}
-            <a className="text-xs font-barlowLight lg:text-[0.6rem]" href="tel:+91 9991831473">+91 9991831473</a>
+            <a className="text-xs font-barlowLight lg:text-[0.6rem] duration-300 hover:translate-x-1" href="tel:+91 9991831473">+91 9991831473</a>
           </p>
           <p className="flex items-center gap-2 lg:gap-1">
             <span>
               <IoIosMail color="#FF6767" />
             </span>{" "}
-            <a className="text-xs font-barlowLight lg:text-[0.6rem]" href="mailto:praveshsaini.dev@gmail.com">
+            <a className="text-xs font-barlowLight lg:text-[0.6rem] duration-300 hover:translate-x-1" href="mailto:praveshsaini.dev@gmail.com">
               praveshsaini.dev@gmail.com
             </a>
           </p>
         </div>
         <div className="flex flex-col gap-1">
           <h2 className="text-xl">Customer Care</h2>
-          <a className="text-xs font-barlowLight lg:text-[0.9rem] lg:tracking-wider lg:font-barlowLight" href="#">Returns</a>
-          <a className="text-xs font-barlowLight lg:text-[0.9rem] lg:tracking-wider lg:font-barlowLight" href="#">Shipping</a>
-          <a className="text-xs font-barlowLight lg:text-[0.9rem] lg:tracking-wider lg:font-barlowLight" href="#">Size Guide</a>
-          <a className="text-xs font-barlowLight lg:text-[0.9rem] lg:tracking-wider lg:font-barlowLight" href="#">Track Orders</a>
-          <a className="text-xs font-barlowLight lg:text-[0.9rem] lg:tracking-wider lg:font-barlowLight" href="#">My Orders</a>
+          <a className="footer_care_and_links" href="#">Returns</a>
+          <a className="footer_care_and_links" href="#">Shipping</a>
+          <a className="footer_care_and_links" href="#">Size Guide</a>
+          <a className="footer_care_and_links" href="#">Track Orders</a>
+          <a className="footer_care_and_links" href="#">My Orders</a>
         </div>
         <div className="flex flex-col gap-1">
           <h2 className="text-xl">Quick Links</h2>
-          <a className="text-xs font-barlowLight lg:text-[0.9rem] lg:tracking-wider lg:font-barlowLight" href="#">About Us</a>
-          <a className="text-xs font-barlowLight lg:text-[0.9rem] lg:tracking-wider lg:font-barlowLight" href="#">Blogs</a>
-          <a className="text-xs font-barlowLight lg:text-[0.9rem] lg:tracking-wider lg:font-barlowLight" href="#">Shop</a>
-          <a className="text-xs font-barlowLight lg:text-[0.9rem] lg:tracking-wider lg:font-barlowLight" href="#">Contact Us</a>
+          <a className="footer_care_and_links" href="#">About Us</a>
+          <a className="footer_care_and_links" href="#">Blogs</a>
+          <a className="footer_care_and_links" href="#">Shop</a>
+          <a className="footer_care_and_links" href="#">Contact Us</a>
         </div>
         <div className="hidden sm:flex"></div>
       </div>
@@ -56,11 +56,11 @@ function Footer() {
         <img className="w-16 sm:w-24" src={logoName} alt="Peer Shop" />
         <p className="text-[0.45rem] text-white/60 sm:text-[0.6rem] sm:tracking-widest">peerShop © 2024 All Rights Reserved</p>
         <div className="flex items-center gap-1 sm:gap-2">
-            <span className="text-[0.7rem] sm:text-[0.9rem]"><AiFillInstagram /></span>
-            <span className="text-[0.7rem] sm:text-[0.9rem]"><MdOutlineFacebook /></span>
-            <span className="text-[0.7rem] sm:text-[0.9rem]"><FaYoutube /></span>
-            <span className="text-[0.7rem] sm:text-[0.9rem]"><FaLinkedin /></span>
-            <span className="text-[0.7rem] sm:text-[0.9rem]"><FaXTwitter /></span>
+            <span className="footer_social-icons"><AiFillInstagram /></span>
+            <span className="footer_social-icons"><MdOutlineFacebook /></span>
+            <span className="footer_social-icons"><FaYoutube /></span>
+            <span className="footer_social-icons"><FaLinkedin /></span>
+            <span className="footer_social-icons"><FaXTwitter /></span>
         </div>
       </div>
     </div>
