46e72b97b65d224b14f831d891b976c36674ca85
pravesh dav <humantorch1932@gmail.com>
2024-05-25 16:48:16 +0530

    added blogs data dynamically, added like dislike UI functionality and done responsiveness

--- src/Components/Blogs.jsx
+++ src/Components/Blogs.jsx
@@ -9,9 +9,6 @@ import { BiSolidLike } from "react-icons/bi";
 import { BiSolidDislike } from "react-icons/bi";
 
 function Blogs() {
-  const [isLiked, setIsLiked] = useState(false);
-  const [isDisLiked, setIsDisLiked] = useState(false);
-
   const blogs = [
     {
       img: blog1,
@@ -27,7 +24,28 @@ function Blogs() {
       perferendis obcaecati ipsam nobis iure quis soluta earum doloribus
       inventore. Officia.`,
     },
+    {
+      img: blog3,
+      name: "Lorem Ipsum",
+      para: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+      perferendis obcaecati ipsam nobis iure quis soluta earum doloribus
+      inventore. Officia.`,
+    },
   ];
+
+  const [likedState, setLikedState] = useState(blogs.map(()=>({liked: false, disliked: false})))
+
+  const handleLike = (index) => {
+    setLikedState(likedState.map((state, i)=>
+      i === index ? {liked: !state.liked, disliked: false}: state
+    ));
+  };
+  const handleDislike = (index) => {
+    setLikedState(likedState.map((state, i)=>
+      i === index ? {liked: false, disliked: !state.disliked}: state
+    ));
+  };
+
   return (
     <div className="w-full bg-[#111] py-4 lg:py-16">
       <div className="w-full h-24 relative sm:h-36 lg:h-60 xl:h-80">
@@ -41,43 +59,40 @@ function Blogs() {
           </button>
         </div>
       </div>
-      <div className="w-full flex flex-col items-center py-6">
-        <h2 className="text-base font-krona mb-3">Our Latest Blogs</h2>
-        <div className="flex gap-1">
+      <div className="w-full flex flex-col items-center py-6 lg:py-10">
+        <h2 className="text-base font-krona mb-3 lg:text-xl lg:mb-7">Our Latest Blogs</h2>
+        <div className="w-[19.5rem] flex gap-1 flex-wrap sm:gap-2 lg:gap-8 xs:w-auto">
           {blogs.map((blog, index) => {
             return (
-              <div className="w-36 h-48 bg-[#090909] flex flex-col gap-1 items-start justify-between p-2 sm:w-52 sm:h-[17rem] lg:w-60 lg:h-80 relative">
-                <img
-                  className="w-32 sm:w-48 lg:w-56"
-                  src={blog.img}
-                  alt=""
-                />
-                <h2 className="text-[0.8rem] sm:text-[0.58rem] lg:text-[0.7rem]">
+              <div
+                key={index}
+                className="w-[9.4rem] min-h-52 bg-[#090909] flex flex-col gap-1 items-start justify-between p-2 sm:w-52 sm:gap-3 lg:w-60 xl:w-72 xl:p-3 relative"
+              >
+                <img className="w-36 sm:w-48 lg:w-56 xl:w-[17rem]" src={blog.img} alt="" />
+                <h2 className="text-[0.8rem] sm:text-[1.2rem] lg:text-[1.5rem] xl:text-[1.7rem]">
                   {blog.name}
                 </h2>
-                <p className="text-[0.4rem]">
-                  {blog.para}
-                </p>
+                <p className="text-[0.4rem] sm:text-[0.53rem] lg:text-[0.65rem] xl:text-[0.75rem]">{blog.para}</p>
                 <div className="w-full flex items-center justify-between">
-                  <button className="text-[0.5rem] bg-[#A80000] px-2 py-[0.15rem] rounded-sm sm:text-[0.4rem] sm:px-4 sm:py-[0.4rem] hover:bg-[#a8000089] active:scale-95 duration-300">
+                  <button className="text-[0.5rem] bg-[#A80000] px-2 py-[0.15rem] rounded-sm sm:text-[0.7rem] sm:px-3 sm:py-[0.2rem] xl:text-[0.85rem] hover:bg-[#a8000089] active:scale-95 duration-300">
                     Read more
                   </button>
-                  <div className="flex gap-1">
+                  <div className="flex gap-1 lg:gap-2">
                     <span
+                    className="cursor-pointer lg:text-xl"
                       onClick={() => {
-                        setIsLiked(true);
-                        setIsDisLiked(false);
+                        handleLike(index);
                       }}
                     >
-                      {isLiked ? <BiSolidLike /> : <BiLike />}
+                      {likedState[index].liked ? <BiSolidLike color="blue" /> : <BiLike />}
                     </span>{" "}
                     <span
+                    className="cursor-pointer lg:text-xl"
                       onClick={() => {
-                        setIsDisLiked(true);
-                        setIsLiked(false);
+                        handleDislike(index);
                       }}
                     >
-                      {isDisLiked ? <BiSolidDislike /> : <BiDislike />}
+                      {likedState[index].disliked ? <BiSolidDislike color="gray" /> : <BiDislike />}
                     </span>
                   </div>
                 </div>
