dea54bb63b407988e295f36fe595ddf278a8d279
pravesh dav <humantorch1932@gmail.com>
2024-05-26 13:54:23 +0530

    replace search bar from header to new arrival section and added search functionality, changed header ui, fetched data from dummy json api

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,34 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
 
 import logo from "../assets/images/logo.png";
-import search from "../assets/images/Icons/search.png";
 import cart from "../assets/images/Icons/cart.png";
 import menu from "../assets/images/Icons/menu.png";
 import logoName from "../assets/images/logo-name.png";
 import close from "../assets/images/Icons/close.png";
+import profile from '../assets/images/profile.jpg'
+import profileDef from '../assets/images/profileDef.svg'
+
 
 function Header() {
   const [isNavHidden, setIsNavHidden] = useState(true);
-  const [searchInput, setSearchInput] = useState('');
-  const [showSearch, setShowSearch] = useState(false);
-  const searchRef = useRef(null);
-
-  const handleDocumentClick = (e) =>{
-    if(searchRef.current && !searchRef.current.contains(e.target)){
-      setShowSearch(false);
-    }
-  }
-  useEffect(() => {
-    if(showSearch){
-      document.addEventListener('mousedown', handleDocumentClick)
-    }
-    else{
-      document.removeEventListener('mousedown', handleDocumentClick)
-    }
-    return () => {
-      document.removeEventListener('mousedown', handleDocumentClick)
-    }
-  }, [showSearch]);  
 
   return (
     <>
@@ -46,34 +28,16 @@ function Header() {
         </nav>
       </div>
       <div className="w-full py-5 px-3 flex items-center justify-between fixed top-0 left-0 z-[98] lg:px-12 lg:py-6 bg-gradient-to-t from-transparent to-black backdrop-blur-sm">
-        <img src={logo} alt="logo image" className="w-6 lg:w-10" />
-        <nav className={`desktop_nav gap-14 hidden lg:flex lg:${showSearch ? 'hidden' : ''}`}>
+        {/* <img src={logo} alt="logo image" className="w-6 lg:w-10" /> */}
+          <span className="w-8 h-8 rounded-full overflow-hidden lg:w-12 lg:h-12"><img className="w-full h-full object-cover" src={profileDef} alt="" /></span>
+          <h2 className="font-krona uppercase text-sm lg:hidden">PeerShop</h2>
+        <nav className={`desktop_nav gap-14 hidden lg:flex`}>
           <a className="text-sm hover:scale-105 duration-300 hover:text-cyan-200" href="#">Home</a>
           <a className="text-sm hover:scale-105 duration-300 hover:text-cyan-200" href="#">About Us</a>
           <a className="text-sm hover:scale-105 duration-300 hover:text-cyan-200" href="#">Shop</a>
           <a className="text-sm hover:scale-105 duration-300 hover:text-cyan-200" href="#">Blog</a>
         </nav>
-        <div
-          ref={searchRef}
-          className={`search-bar w-[60%] h-8 flex justify-between items-center border border-white/30 bg-white/10 rounded-3xl lg:absolute lg:left-1/2 lg:-translate-x-1/2 lg:w-[45%] lg:h-10 px-5 duration-500 ${showSearch ? 'lg:scale-[1]' : 'lg:scale-0'}`}
-        >
-          <input
-            className="w-[80%] h-full pl-1 border-none outline-none bg-transparent text-xs ml-1 lg:text-sm"
-            type="text"
-            name="search_item"
-            id="search_item"
-            placeholder="Search items here"
-            value={searchInput}
-            onChange={(e) => {
-              let search = e.target.value;
-              setSearchInput(search);
-            }}
-          />
-          <img className={`w-4 mr-2 cursor-pointer ${searchInput !== "" ? 'hidden' : 'block'}`} src={search} alt="search icon" />
-          <img className={`w-4 mr-2 cursor-pointer ${searchInput !== "" ? 'block' : 'hidden'}`} src={close} alt="close png" onClick={() => { setSearchInput('') }} />
-        </div>
         <div className="flex items-center gap-3 lg:gap-7">
-          <img className={`hidden cursor-pointer lg:block lg:${showSearch ? 'hidden' : ''} lg:w-5`} src={search} alt="search icon" onClick={() => { setShowSearch(true) }} />
           <div className="relative">
             <img className="w-4 lg:w-6 cursor-pointer" src={cart} alt="cart icon" />
             <span className="absolute -top-[40%] -right-[30%] bg-red-700 w-3 h-3 rounded-full flex justify-center items-center text-[0.55rem] lg:w-4 lg:h-4 lg:text-[0.65rem]">4</span>
--- src/Components/TopRated.jsx
+++ src/Components/TopRated.jsx
@@ -13,9 +13,9 @@ function TopRated() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        let response = await fetch("/topRated.json");
+        let response = await fetch("https://dummyjson.com/products");
         let data = await response.json();
-        setItems(data);
+        setItems(data.products);
         setLoading(false);
       } catch (err) {
         console.log(err);
@@ -34,32 +34,36 @@ function TopRated() {
   if (error) return <p>There was an error loading the new arrivals.</p>;
 
   return (
+    <>
     <div className="w-full p-2 flex flex-wrap justify-center gap-2 font-krona lg:justify-start lg:w-[61.6rem] lg:p-0 xl:w-[65.5rem] xl:gap-7">
       {items.map((item, index) => {
-        return (
-          <div key={index} className="w-36 h-48 bg-[#090909] flex flex-col items-start justify-between p-3 sm:w-52 sm:h-[17rem] lg:w-60 lg:h-80 relative">
-            <img className="w-32 sm:w-48 lg:w-56" src={item.img} alt={item.name} />
-            <img
-              className="w-32 sm:w-48 lg:w-56 absolute top-2 left-2 hover:scale-105 duration-300"
-              src={item.img}
-              alt={item.name}
-              onClick={() => {
-                setIsClicked(true);
-                handelClickedImage(item.img);
-              }}
-            />
-            <h2 className="text-[0.38rem] sm:text-[0.58rem] lg:text-[0.7rem]">{item.name}</h2>
-            <button className="bg-[#4f717949] text-[#3598AF] text-[0.45rem] px-2 py-[0.04rem] rounded-sm sm:text-[0.60rem] sm:px-4 sm:py-[0.14rem]">
-              {item.rating}
-            </button>
-            <div className="w-full flex justify-between items-center">
-              <span className="text-[0.52rem] sm:text-[0.8rem]">${item.price}</span>{" "}
-              <button className="text-[0.3rem] bg-[#0039CA] px-3 py-[0.3rem] rounded-sm sm:text-[0.4rem] sm:px-4 sm:py-[0.4rem] hover:bg-[#0039cac9]">
-                Add to cart
+        if(item.rating > 4.5){
+          return (
+            <div key={index} className="w-36 h-48 bg-[#090909] flex flex-col items-start justify-between p-3 sm:w-52 sm:h-[17rem] lg:w-60 lg:h-80 relative">
+              {/* <img className="w-32 sm:w-48 lg:w-56" src={item.thumbnail} alt={item.title} /> */}
+              <img
+                className="w-32 sm:w-48 lg:w-56  hover:scale-105 duration-300"
+                src={item.thumbnail}
+                alt={item.title}
+                onClick={() => {
+                  setIsClicked(true);
+                  handelClickedImage(item.thumbnail);
+                }}
+              />
+              <h2 className="text-[0.38rem] sm:text-[0.58rem] lg:text-[0.7rem]">{item.title}</h2>
+              <button className="bg-[#4f717949] text-[#3598AF] text-[0.45rem] px-2 py-[0.04rem] rounded-sm sm:text-[0.60rem] sm:px-4 sm:py-[0.14rem]">
+                {item.rating}
               </button>
+              <div className="w-full flex justify-between items-center">
+                <span className="text-[0.52rem] sm:text-[0.8rem]">${item.price}</span>
+                <button className="text-[0.3rem] bg-[#0039CA] px-3 py-[0.3rem] rounded-sm sm:text-[0.4rem] sm:px-4 sm:py-[0.4rem] hover:bg-[#0039cac9]">
+                  Add to cart
+                </button>
+              </div>
             </div>
-          </div>
-        );
+          );
+        }
+        
       })}
       <div
         className={`w-full h-screen fixed top-0 left-0 bg-black/10 backdrop-blur-lg z-[99] justify-center items-center hidden lg:${
@@ -72,13 +76,14 @@ function TopRated() {
         }} />
         <div className="w-[30rem] h-[30rem] filter drop-shadow-[0_0_5px_#ffffff23]">
           <img
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover bg-[#111]"
             src={clickedImage}
             alt="clicked Images"
           />
         </div>
       </div>
     </div>
+    </>
   );
 }
 
