09c06b198a6b63ed68945e6a14745aa48d99dfbd
pravesh dav <humantorch1932@gmail.com>
2024-06-12 13:03:20 +0530

    added design for added address

--- src/Components/Address.jsx
+++ src/Components/Address.jsx
@@ -1,31 +1,60 @@
-import React, { useState } from 'react'
-import { FaAngleLeft } from 'react-icons/fa6'
-import { RiAddFill } from 'react-icons/ri'
-import AddNewAddress from './AddNewAddress'
+import React, { useState } from "react";
+import { FaAngleLeft } from "react-icons/fa6";
+import { RiAddFill } from "react-icons/ri";
+import AddNewAddress from "./AddNewAddress";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 
 function Address() {
-    const [isAddAddress, setIsAddAddress] = useState(false)
-    const handleAddAddress = () =>{
-        setIsAddAddress(true);
-    }
+  const [isAddAddress, setIsAddAddress] = useState(false);
+  const handleAddAddress = () => {
+    setIsAddAddress(true);
+  };
   return (
-    <div className='w-full h-full bg-[#111] relative'>
+    <div className="w-full h-full bg-[#111] relative">
       <div className="flex items-center gap-5">
         <span className="text-lg">
           <FaAngleLeft />
         </span>
         <h1 className="text-white text-xl">Manage Addresses</h1>
       </div>
-      <div className={`w-full py-2 px-4 mt-5 text-[#3F3BFF] uppercase gap-3 text-sm items-center border-[0.5px] border-white/40 ${isAddAddress ? 'hidden' : 'flex'}`} onClick={handleAddAddress}>
-        <span className='text-xl'><RiAddFill/></span>
+      <div
+        className={`w-full py-2 px-4 mt-5 text-[#3F3BFF] uppercase gap-3 text-sm items-center border-[0.5px] border-white/40 ${
+          isAddAddress ? "hidden" : "flex"
+        }`}
+        onClick={handleAddAddress}
+      >
+        <span className="text-xl">
+          <RiAddFill />
+        </span>
         Add a new address
       </div>
-      <div className={`w-full h-[32rem] ${isAddAddress ? 'block' : 'hidden'} overflow-y-auto`}>
-        <AddNewAddress/>
+      <div
+        className={`w-full h-[32rem] ${
+          isAddAddress ? "block" : "hidden"
+        } overflow-y-auto`}
+      >
+        <AddNewAddress />
       </div>
 
+      <div className="w-full p-1 py-2 border-[0.5px] border-white/40 mt-5">
+        <div className="flex justify-between items-center">
+          <span className="bg-[#282828] px-3 py-1 text-[#7B7B7B] text-[0.5rem]">
+            Home
+          </span>{" "}
+          <span className="text-[#7B7B7B] text-sm">
+            <HiOutlineDotsVertical />
+          </span>
+        </div>
+        <h2 className="flex gap-5 text-sm">
+          Pravesh <span className="font-sans font-thin">9991831473</span>
+        </h2>
+        <p className="text-[0.52rem] text-[#b8b8b8] w-64">
+          Shree Balaji mandir, Niwaz Nagar, Mahendragarh District, Haryana -{" "}
+          <span className="text-white">123001</span>
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Address
+export default Address;
